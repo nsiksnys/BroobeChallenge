1d33e5723913f5672872e90520591b68a1baf978
Nadia Siksnys <nadiasiksnys@gmail.com>
2024-11-26 20:01:20 -0300

    Installed DataTables

--- resources/views/metrics/list.blade.php
+++ resources/views/metrics/list.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <!-- list metrics -->
     <h1> Metric history</h1>
-    <table class="table">
+    <table id="resultsTable" class="table">
         <thead>
             <th>ID</th>
             <th>URL</th>
@@ -14,6 +14,7 @@
         </thead>
         <tbody>
             @forelse ($records as $record)
+            <tr>
                 <td>{{ $record->id }}</td>
                 <td>{{ $record->url }}</td>
                 <td>{{ $record->accesibility_metric }}</td>
@@ -22,8 +23,26 @@
                 <td>{{ $record->pwa_metric }}</td>
                 <td>{{ $record->seo_metric }}</td>
                 <td>{{ $record->created_at }}</td>
+            </tr>
             @empty
+            <tr>
                 <td colspan="8" class="text-center">No records available</td>
+            </tr>
             @endforelse
+        </tbody>
     </table>
+    <script>
+        $(document).ready( function () {
+            $('#resultsTable').DataTable({
+                // Hide the id column
+                columnDefs: [
+                    {
+                        target: 0,
+                        visible: false,
+                        searchable: false
+                    }
+                ]
+            });
+        } );
+    </script>
 </x-layout>
