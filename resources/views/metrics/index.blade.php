1c3ba26db9b879262c2ef9e788ff587db432a43a
Nadia Siksnys <nadiasiksnys@gmail.com>
2024-12-01 16:35:54 -0300

    Card component
    Created a separated Card component, instead of rendering them directly in the index view.

--- resources/views/metrics/index.blade.php
+++ resources/views/metrics/index.blade.php
@@ -60,12 +60,8 @@
                 {{-- create category cards (all but PWA) --}}
                 @foreach ($categories as $category)
                 @if ($category->name != "PWA")
-                <div id="{{ strtolower($category->name) }}-card" name="{{ strtolower($category->name) }}-card" class="card text-center col-md-2 me-5">
-                    <div id="{{ strtolower($category->name) }}-card" name="{{ strtolower($category->name) }}-card" class="card-body">
-                        <h6 class="card-subtitle mb-2 text-body-secondary">{{ str_replace('_',' ',$category->name) }}</h6>
-                        <h1 id="{{ strtolower($category->name) }}-card-value" class="card-title">0.0</h1>
-                    </div>
-                </div>
+                <x-card :category="$category->name">
+                </x-card>
                 @endif
                 @endforeach
             </div>
